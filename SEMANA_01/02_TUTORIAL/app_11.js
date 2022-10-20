8b299d94bf28120c10ad98d45b7acd74308b88ed
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2022-10-20 12:12:48 -0300

    oi

--- SEMANA_01/02_TUTORIAL/app_11.js
+++ SEMANA_01/02_TUTORIAL/app_11.js
@@ -1,13 +1,13 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.1'; //localhost
 const port = 3011;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \
+  res.end('<!DOCTYPE html> <head> <meta charset="utf-8"> <title>Etapa 0 - INSTALACÃO</title></head> \
               <body>\
                 <div id="main"> \
-                       <h1> Etapa 1 - INSTALACAO - Servidor Node.js </h1> \
+                       <h1> Etapa 1 - INSTALACÃO - Servidor Node.js </h1> \
                        <H2> Meu servidor NODE.js funciona!</H2> </div> \
               </body> \
            </html>');
