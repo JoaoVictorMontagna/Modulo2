fdc72ac1931409201e601f0f25ea7a220b6c7f67
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2022-11-23 10:33:20 -0300

    ex aula 6

---
+++ SEMANA_06/04_AUT_EST_EX_OBRIGATORIOS/script.js
@@ -0,0 +1,19 @@
+﻿
+$(document).ready(function(){
+    $("#exp").click(function(){
+      $("#cont").slideToggle("slow");
+      $("#cont2").slideToggle("slow");
+    });
+  });
+
+
+$(document).ready(function(){
+    $("#botao").click(function(){
+      $("#hab1").fadeToggle("slow");
+      $("#hab2").fadeToggle("slow");
+      $("#hab3").fadeToggle("slow");
+    });
+  });
+
+
+
