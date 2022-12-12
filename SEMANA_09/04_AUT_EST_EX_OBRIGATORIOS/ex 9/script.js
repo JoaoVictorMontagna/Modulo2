7ac437c7f90996fed8474ea1b4acf833e423a18d
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2022-12-12 15:57:08 -0300

    atualização

--- SEMANA_09/04_AUT_EST_EX_OBRIGATORIOS/ex 9/script.js
+++ SEMANA_09/04_AUT_EST_EX_OBRIGATORIOS/ex 9/script.js
@@ -41,3 +41,15 @@ function jurosimples(){
 
 
 }
+
+function impressaoMaluca(){
+    texto = document.getElementById("texto").value;
+    divSaida = document.getElementById("saida");
+    saida = '';
+    espacos = '';
+    for (let caractere of texto){
+        saida += '<br>' + espacos + caractere;
+        espacos += '&nbsp &nbsp';
+    }
+    divSaida.innerHTML = saida;
+}
